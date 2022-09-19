c8c99b00a1b00ef7c977b3ccfa1e5c8cb243f28d
shourovahmd <moh.abuhanifa@gmail.com>
2022-09-19 13:44:24 +0600

    socket separeted

--- server/server.js
+++ server/server.js
@@ -13,20 +13,26 @@ const router = jsonServer.router("db.json");
 
 // response middleware
 router.render = (req, res) => {
-    const path = req.path;
-    const method = req.method;
+  const path = req.path;
+  const method = req.method;
 
-    if (
-        path.includes("/conversations") &&
-        (method === "POST" || method === "PATCH")
-    ) {
-        // emit socket event
-        io.emit("conversation", {
-            data: res.locals.data,
-        });
-    }
+  if (
+    path.includes("/conversations") &&
+    (method === "POST" || method === "PATCH")
+  ) {
+    // emit socket event for the conversations
+    io.emit("conversation", {
+      data: res.locals.data,
+    });
+  }
+  if (path.includes("/messages") && method === "POST") {
+    // emit socket event for the messages  
+    io.emit("message", {
+      data: res.locals.data,
+    });
+  }
 
-    res.json(res.locals.data);
+  res.json(res.locals.data);
 };
 
 const middlewares = jsonServer.defaults();
@@ -38,9 +44,9 @@ app.db = router.db;
 app.use(middlewares);
 
 const rules = auth.rewriter({
-    users: 640,
-    conversations: 660,
-    messages: 660,
+  users: 640,
+  conversations: 660,
+  messages: 660,
 });
 
 app.use(rules);
